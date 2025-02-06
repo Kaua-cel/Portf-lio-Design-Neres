701552436d5a61a71f3f357e1c63965899ea58be
Neres-Dev <neresdesenho16@gmail.com>
2025-02-06 19:09:45 -0300

    Finalizing the menu

--- menu.js
+++ menu.js
@@ -1,1 +1,15 @@
-let bttMenu = document.getElementById
+let btnMenu = document.getElementById("btnMenu")
+let menu = document.getElementById("menuMobile")
+let overlay = document.getElementById("overlayMenu")
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('openMenu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('openMenu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('openMenu')
+})
